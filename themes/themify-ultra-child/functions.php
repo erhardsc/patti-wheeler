541a5b17fca9fe7cd7cf3a713edf829d191466d2
Sumner <serhard2556@gmail.com>
2017-09-09 02:40:47 -0600

    starting home page. Added a sweet door animation.

--- themes/themify-ultra-child/functions.php
+++ themes/themify-ultra-child/functions.php
@@ -10,8 +10,27 @@ add_action( 'wp_enqueue_scripts', 'themify_custom_enqueue_child_theme_styles', 1
 
 function custom_styles() {
 
-	wp_register_script('main-js', get_stylesheet_directory_uri() . '/js/main.js', array('jquery'), false);
+	wp_register_style('door-css', get_stylesheet_directory_uri() . '/css/door.css' );
 
-	wp_enqueue_script('main-js');
+	if( is_front_page() ) {
+
+			wp_enqueue_style( 'door-css' );
+
+	}
 }
 add_action( 'wp_enqueue_scripts', 'custom_styles' );
+
+function custom_scripts() {
+
+	wp_register_script('main-js', get_stylesheet_directory_uri() . '/js/main.js', array('jquery'), false);
+	wp_register_script('door-js', get_stylesheet_directory_uri() . '/js/door.js', array('jquery'), false);
+
+	wp_enqueue_script('main-js');
+
+	if( is_front_page() ){
+
+		wp_enqueue_script('door-js');
+
+	}
+}
+add_action( 'wp_enqueue_scripts', 'custom_scripts' );
